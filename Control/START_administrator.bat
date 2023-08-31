c615e3e9946fa3807b52445ef0656b9817db2139
KadotaMasayuki <44630941+KadotaMasayuki@users.noreply.github.com>
2023-08-31 11:50:30 +0900

    Add files via upload

---
+++ Control/START_administrator.bat
@@ -0,0 +1,7 @@
+@echo off
+@rem start
+@rem need administrator
+
+sc  start  IoTDiceLoggingService
+
+pause
---
+++ Control/STOP_administrator.bat
@@ -0,0 +1,7 @@
+@echo off
+@rem stop
+@rem need administrator
+
+sc  stop  IoTDiceLoggingService
+
+pause
